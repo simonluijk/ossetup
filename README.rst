922ab4b81c1867af64f489dd6c838c463152398f
Simon Luijk <simon@luijk.co.uk>
2010-09-21 15:51:34 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+========
+ osSetup
+========
+
+osSetup is a collection of scripts I use to setup my fresh os installs.
---
+++ ubuntu-desktop.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+# Dropbox
+cd ~ && wget http://dl-web.dropbox.com/u/17/dropbox-lnx.x86-0.8.105.tar.gz && killall dropbox; rm -rf .dropbox-dist/ && tar xzf dropbox* && rm dropbox*
+dropbox start
+
+# Plugins
+sudo apt-get -y install gstreamer0.10-ffmpeg gedit-plugins
+
+# DVCS and svn
+sudo apt-get -y install git-gui git-doc git-svn mercurial subversion
+
+# Fonts
+sudo apt-get -y install ttf-mscorefonts-installer ttf-dejavu ttf-larabie-deco ttf-larabie-straight ttf-larabie-uncommon gnome-specimen
+
+# Ntp
+sudo apt-get -y install ntp
+if !(grep -q '^## Pool servers' /etc/ntp.conf); then
+    sudo sh -c 'echo "" >> /etc/ntp.conf'
+    sudo sh -c 'echo "## Pool servers" >> /etc/ntp.conf'
+    sudo sh -c 'echo "server 0.europe.pool.ntp.org" >> /etc/ntp.conf'
+    sudo sh -c 'echo "server 1.europe.pool.ntp.org" >> /etc/ntp.conf'
+    sudo sh -c 'echo "server 2.europe.pool.ntp.org" >> /etc/ntp.conf'
+    sudo ntpdate
+    sudo /etc/init.d/ntp restart
+fi
+
+# Python tools
+sudo apt-get -y install python-virtualenv python-pip fabric
+sudo apt-get -y install python-reportlab python-beautifulsoup
+
+# Utilities
+sudo apt-get -y install virtualbox-ose tasque liferea artha cryptsetup
+
+# Vim
+sudo apt-get -y install vim-gnome vim-doc
+
+# Wait for dropbox
+echo -n "Once Dropbox is synced hit [ENTER]: "
+read -n 0 dropbox_done
+
+# Link dotfiles
+chmod +x $HOME/Dropbox/Dotfiles/symlink.py
+$HOME/Dropbox/Dotfiles/symlink.py
