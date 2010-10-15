f77b8f70ef1d7e564c7368f2846bbf81b06eac93
Simon Luijk <simon@luijk.co.uk>
2010-10-15 11:45:02 +0200

    Install getmail4

--- ubuntu-desktop.sh
+++ ubuntu-desktop.sh
@@ -65,7 +65,7 @@ sudo apt-get -y install python-virtualenv python-pip fabric
 sudo apt-get -y install python-reportlab python-beautifulsoup python-libcloud
 
 # Utilities
-sudo apt-get -y install virtualbox-ose tasque liferea artha cryptsetup sqlite3 sqlite3-doc conduit istanbul contacts keepass hamster-applet
+sudo apt-get -y install virtualbox-ose tasque liferea artha cryptsetup sqlite3 sqlite3-doc conduit istanbul contacts keepass hamster-applet getmail4
 
 # Vim
 sudo apt-get -y install vim-gnome vim-doc
